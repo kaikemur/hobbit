5a9f66638dc5174568c5e196a724a12c130dc1b3
kaikez <kaike.ribeiro@aluno.senai.br>
2025-08-22 11:40:28 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,76 @@
+import express from "express"
+import personagens from "./src/data/personagens.js";
+
+
+const app = express();
+app.use(express.json());
+
+const serverPort = 3003;
+
+app.get("",(req,res) => {
+    res.send("bem vindo ao servidor de hobbit")
+})
+
+app.get("/personagens",(req,res) => {
+    res.json(personagens);
+})
+
+
+app.get("/personagens/:id", (req,res) => {
+
+    const id = parseInt(req.params.id);
+
+    const personagen = personagens.find(b => b.id === id);
+
+    if(personagen){
+        res.status(200).json(personagen)
+    } else{
+        res.status(404).json(personagen)({
+            mensagen:"personagen não encontrados"
+        })
+    }
+})
+
+
+app.get("/personagens/nome/:nome", (req,res) => {
+    let nome = req.params.nome
+    nome = nome.toLowerCase()
+
+    const nomesFiltrados = personagens.find(p => p.nome.toLowerCase().includes(nome))
+
+    if(nomesFiltrados){
+        res.status(200).json(nomesFiltrados);
+    }else{
+        res.status(404).json({
+            mensagem:"personagen não encontrado!"
+        })
+    }
+})
+app.get("/personagens/raca/:raca", (req,res) => {
+    let raca = req.params.raca
+    raca = toLowerCase()
+
+    const racasFiltrados = personagens.filter(p => p.raca.toLowerCase().includes(raca))
+
+    if(racasFiltrados){
+        res.status(200).json(racasFiltrados);
+    }else{
+        res.status(404).json({
+            mensagem:"raça não encontrado!"
+        })
+    }
+
+})
+
+app.get("/personagens/vivos/sim", (req,res) => {
+    const personagensVivos = personagens.filter(p => p.vivo === true)
+    if(personagensVivos){
+        res.status(200).json(personagensVivos)
+    }
+})
+
+
+
+app.listen(serverPort, () => {
+    
+});
---
+++ src/data/personagens.js
@@ -0,0 +1,103 @@
+export default[
+  {
+    "id": 1,
+    "nome": "Bilbo Bolseiro",
+    "titulo": "O Ladrão",
+    "raca": "Hobbit",
+    "afiliacao": "Companhia de Thorin",
+    "arma": "Ferroada",
+    "descricao": "O protagonista da história, um hobbit pacato que se junta a uma aventura inesperada.",
+    "vivo": true
+  },
+  
+  {
+    "id": 2,
+    "nome": "Thorin Escudo de Carvalho",
+    "titulo": "Rei sob a Montanha",
+    "raca": "Anão",
+    "afiliacao": "Companhia de Thorin",
+    "arma": "Orcrist",
+    "descricao": "Líder da companhia de anões, herdeiro do trono da Montanha Solitária.",
+    "vivo": false
+  },
+  {
+    "id": 3,
+    "nome": "Smaug",
+    "titulo": "O Terrível",
+    "raca": "Dragão",
+    "afiliacao": "Montanha Solitária",
+    "arma": "Fogo e escamas",
+    "descricao": "O grande dragão que roubou o tesouro dos anões da Montanha Solitária.",
+    "vivo": false
+  },
+  {
+    "id": 4,
+    "nome": "Bard o Arqueiro",
+    "titulo": "Matador de Dragões",
+    "raca": "Homem",
+    "afiliacao": "Esgaroth (Cidade do Lago)",
+    "arma": "Flecha Negra",
+    "descricao": "Um arqueiro de Esgaroth que confronta Smaug.",
+    "vivo": true
+  },
+  {
+    "id": 5,
+    "nome": "Gandalf",
+    "titulo": "O Mago",
+    "raca": "Istari (Mago)",
+    "afiliacao": "A Ordem dos Magos",
+    "arma": "Cajado e Glamdring",
+    "descricao": "Um dos magos que guia Bilbo e os anões em sua jornada.",
+    "vivo": true
+  },
+  {
+    "id": 6,
+    "nome": "Beorn",
+    "titulo": "O Troca-Peles",
+    "raca": "Homem (Troca-Peles)",
+    "afiliacao": "Nenhum",
+    "arma": "Força física",
+    "descricao": "Um homem que pode se transformar em um urso gigante.",
+    "vivo": true
+  },
+  {
+    "id": 7,
+    "nome": "Gollum",
+    "titulo": "Sméagol",
+    "raca": "Hobbit-alterado",
+    "afiliacao": "Montanhas Sombrias",
+    "arma": "Nenhum",
+    "descricao": "Uma criatura corrompida pelo Um Anel, que o chama de 'Precioso'.",
+    "vivo": false
+  },
+  {
+    "id": 8,
+    "nome": "Elrond",
+    "titulo": "Senhor de Valfenda",
+    "raca": "Elfo",
+    "afiliacao": "Valfenda",
+    "arma": "Nenhum",
+    "descricao": "Um dos mais sábios e antigos elfos, que oferece refúgio e conselho.",
+    "vivo": true
+  },
+  {
+    "id": 9,
+    "nome": "O Rei Élfico (Thranduil)",
+    "titulo": "Rei da Floresta das Trevas",
+    "raca": "Elfo",
+    "afiliacao": "Floresta das Trevas",
+    "arma": "Nenhum",
+    "descricao": "O rei dos elfos da Floresta das Trevas, pai de Legolas.",
+    "vivo": true
+  },
+  {
+    "id": 10,
+    "nome": "Bolg",
+    "titulo": "Rei dos Goblins",
+    "raca": "Orc",
+    "afiliacao": "Montanhas Sombrias",
+    "arma": "Machado",
+    "descricao": "O orc líder, filho de Azog, que persegue Thorin e sua companhia.",
+    "vivo": false
+  }
+];
